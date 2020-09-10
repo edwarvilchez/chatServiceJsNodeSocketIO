ad2dcc6a6fe4d386bdc3b45d8768ed813f925e96
edwarvilchez <edwarvilchez1977@gmail.com>
2020-09-10 15:42:17 -0400

    Se crean las carpetas public css js y sus archivos

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 
+
+app.use(express.static('public'));
+
+
+//server area
 app.listen(3000, () => {
     console.log('Servidor activo en el puerto 3000');
 });
